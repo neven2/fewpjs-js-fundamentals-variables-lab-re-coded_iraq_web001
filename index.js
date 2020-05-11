2f9d1f426217ba2f8a5122d98a803e0d844db5aa
Neven Bedr <neven2225@gmail.com>
2020-05-11 10:38:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
 
 const companyName = 'Scuber';
-const mostProfitableNeighborhood="Chelsea"
+let mostProfitableNeighborhood="Chelsea"
 let companyCeo="Susan Smith"
